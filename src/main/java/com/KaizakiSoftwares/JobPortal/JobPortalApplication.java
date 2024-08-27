7aa2372489bf9b8708bfef6454db508e3cb42346
Chamith Dilshan <ssoftware190@gmail.com>
2024-08-27 22:02:17 +0530

    Feature_registration

---
+++ src/main/java/com/KaizakiSoftwares/JobPortal/JobPortalApplication.java
@@ -0,0 +1,32 @@
+package com.KaizakiSoftwares.JobPortal;
+
+import com.KaizakiSoftwares.JobPortal.role.Role;
+import com.KaizakiSoftwares.JobPortal.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableAsync
+public class JobPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPortalApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(RoleRepository roleRepository) {
+		return args -> {
+			if(roleRepository.findByName("USER").isEmpty()){
+				roleRepository.save(
+						Role.builder().name("USER").build()
+				);
+			}
+		};
+	}
+
+}
